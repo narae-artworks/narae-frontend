4ea67bb6448cc6ca002f950bbc08219060643490
LiteHell <litehell@litehell.info>
2024-06-10 14:32:30 +0900

    cover displayed only once

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,31 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import IndexCover from "./index-cover/index-cover";
 import { useRouter } from "next/navigation";
+import Home from "./main/page";
 
-export default function Home() {
+export default function Index() {
   const router = useRouter();
-  
+  const [coverEnabled, setCoverEnabled] = useState<boolean | null>(null);
+  useEffect(() => {
+    if (coverEnabled === null) {
+      if (localStorage.getItem('coverEnabled') === null) {
+        localStorage.setItem('coverEnabled', 'true');
+        setCoverEnabled(true);
+      } else {
+        setCoverEnabled(JSON.parse(localStorage.getItem('coverEnabled') as string))
+      }
+    }
+  }, []);
+
   return (
     <div>
+      { coverEnabled === null ? null : coverEnabled ? 
       <IndexCover onEnter={() => {
-        router.replace("/main");
-      }}></IndexCover>
+        localStorage.setItem('coverEnabled', 'false');
+        setCoverEnabled(false);
+      }}></IndexCover> : <Home></Home>}
     </div>
   );
 }

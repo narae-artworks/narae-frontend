075c77126be9aeb3015ea711fbec440a25c2ec04
asdarwin03 <asdarwin03@gmail.com>
2024-06-11 04:06:53 +0900

    add other/awards prototype

---
+++ src/app/other/awards/awards-sample.ts
@@ -0,0 +1,17 @@
+export type awardsInfo = {
+    id: string,
+    content: string,
+}
+
+const awardssample = [{
+    id: "1",
+    content: "content 1",
+}, {
+    id: "2",
+    content: "content 2",
+}, {
+    id: "3",
+    content: "content 3",
+}]
+
+export default awardssample;

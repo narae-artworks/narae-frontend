219800bf5880a68620403763a3cdce8f1ac16ba4
asdarwin03 <asdarwin03@gmail.com>
2024-06-24 05:57:06 +0900

    delete mobile intro, change video size according to screen width(1024px, 1920px)

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -4,6 +4,8 @@ import TopNavbar from "../top-navbar/top-navbar";
 import nightBackground from '../sky-background.module.scss';
 import ReactFullpage from "@fullpage/react-fullpage";
 import styles from './page.module.scss';
+import Image from 'next/image';
+import naraeWhite from '@/img/logo-white.png';
 
 export default function Home() {
   return (
@@ -17,8 +19,11 @@ export default function Home() {
               <ReactFullpage.Wrapper>
                 <div className="section">
                   <div className={styles.videoContainer}>
-                    <video src="/intro.mp4" autoPlay controls={false}></video>
+                    <video src="/intro.mp4" autoPlay controls={false} ></video>
                   </div>
+                  <div className={styles.logoContainer}>
+                      <Image src={naraeWhite} alt="narae white logo image" className={styles.LogoImg}></Image>
+                    </div>
                 </div>
                 {/* <div className="section">
                   이벤트

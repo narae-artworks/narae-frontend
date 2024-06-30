d04d6c8969f7b614407d43aafa41df80c428a012
LiteHell <litehell@litehell.info>
2024-07-01 00:27:03 +0900

    fix: remove use of fullpage.js
    to avoid gpl issue

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -2,7 +2,6 @@
 
 import TopNavbar from "../top-navbar/top-navbar";
 import nightBackground from '../sky-background.module.scss';
-import ReactFullpage from "@fullpage/react-fullpage";
 import styles from './page.module.scss';
 import Image from 'next/image';
 import naraeWhite from '@/img/logo-white.png';
@@ -11,30 +10,16 @@ export default function Home() {
   return (
     <div className={nightBackground.nightBackground} style={{minHeight: '100vh'}}>
         <TopNavbar className={styles.navbar}></TopNavbar>
-        <ReactFullpage
-          scrollOverflow={false}
-          credits={{enabled: false}}
-          render={({ state, fullpageApi }) => {
-            return (
-              <ReactFullpage.Wrapper>
-                <div className="section">
-                  <div className={styles.videoContainer}>
-                    <video src="/intro.mp4" autoPlay controls={false} ></video>
-                  </div>
-                  <div className={styles.logoContainer}>
-                      <Image src={naraeWhite} alt="narae white logo image" className={styles.LogoImg}></Image>
-                    </div>
-                </div>
-                {/* <div className="section">
-                  이벤트
-                </div>
-                <div className="section">
-                  어워드
-                </div> */ }
-              </ReactFullpage.Wrapper>
-            );
-          }}
-        />
+        <div className={styles.contentContainer}>
+          <div className="section">
+            <div className={styles.videoContainer}>
+              <video src="/intro.mp4" autoPlay controls={false} ></video>
+            </div>
+            <div className={styles.logoContainer}>
+                <Image src={naraeWhite} alt="narae white logo image" className={styles.LogoImg}></Image>
+              </div>
+          </div>
+        </div>
     </div>
   );
 }
